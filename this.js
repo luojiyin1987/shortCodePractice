6e35c176a026bf14ace1339a4aa21183635d0f35
luojiyin <luojiyin@hotmail.com>
2022-08-11 17:42:56 +0800

    code init

---
+++ this.js
@@ -0,0 +1,19 @@
+function foo() {
+    console.log("simple function call")
+    console.log(this === global)
+}
+
+let user = {
+    count: 10,
+    foo: foo,
+    foo1: function() {
+        console.log(this === global)
+    }
+}
+
+
+user.foo()
+let fun1 = user.foo1
+fun1()
+user.foo1()
+
