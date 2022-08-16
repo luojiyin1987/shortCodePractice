45c3df337c94003a9cb8ac199af7fa8dd0718e89
luojiyin <luojiyin@hotmail.com>
2022-08-16 21:49:14 +0800

    use promise all

--- async.js
+++ async.js
@@ -10,10 +10,34 @@ function asyncUpperCase(item) {
 async function uppercaseItems() {
     const items = ['a', 'b', 'c'];
     for (item of items) {
-        console.log(await asyncUpperCase(item));
+        console.log('uppercaseItems' ,await asyncUpperCase(item));
     }
 
-    console.log('Items processed')
+    console.log('Items 1 processed')
+}
+
+
+
+const asyncUpperCase2 = (item) => {
+    return  new Promise(resolve => {
+        setTimeout(() => {
+            resolve(item.toUpperCase())
+        }, Math.floor(Math.random() * 1000))
+
+    })
+}
+
+function uppercaseItems2() {
+    const items = ['a', 'b', 'c'];
+    return Promise.all(
+        items.map(async item => {
+            const uppercaseItems  = await asyncUpperCase2(item);
+            console.log('uppercaseItems2',uppercaseItems);
+        })
+    )
+
+    console.log('Items 2  processed')
 }
 
 uppercaseItems();
+uppercaseItems2();
