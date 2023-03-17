1d0e509d58dc4234be5139c514d012af6e6d2632
luojiyin <luojiyin@hotmail.com>
2023-03-17 11:16:24 +0800

    chore: remove no use

--- quote-generator/pages/index.tsx
+++ quote-generator/pages/index.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 
 interface Quote {
